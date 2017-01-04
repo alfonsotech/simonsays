d47447d5d09505a08bf9398688fd7ed7380bc668
Rose Hip <rosehip@Roses-MacBook-Pro.local>
2017-01-03 17:28:51 -0800

    added basic styles

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const pug = require('pug')
-const normalize = require('normalize')
+const normalized = require('express-normalized')
 
 app.set('view engine', 'pug')
 // app.set('/', __dirname + '/views') //double check this
@@ -9,10 +9,12 @@ app.set('views', __dirname + 'views')
 
 // app.use('/', index)
 app.use(express.static('public'))
+app.use(normalized())
 
 app.get('/', function(request, response) {
   // response.send('Hello Tacos!')
   // response.render(index)
+  console.log(request.normalized);
   response.render(__dirname + '/views/index.pug')
 })
 
