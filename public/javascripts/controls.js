cfb24eba960b9659eabdbbae26c4a4abcc394aca
Helen Yau <helenyau13@gmail.com>
2017-01-04 09:48:07 -0800

    Add sound files and scripts to pug for control.js

---
+++ public/javascripts/controls.js
@@ -0,0 +1,33 @@
+'use strict'
+//array to hold played color-notes
+//array of set of color-notes
+
+//function to randomly select new color-note
+
+//start game function
+  //reset game if already activated
+  //activate loop
+
+//main game loop
+  //run playback function
+  //input function
+  //check error or win condition
+
+  //playback function
+    //play sounds from array
+    //add new sound to array 
+
+  //input function
+    //loop while played array size is less than color-array size
+    //get input clicks
+      //push click to to played array
+
+
+  //play first sound
+  //need to push to the array of played color-notes
+  //listens and checks to see if player gives the right input:
+    //if player makes correct input then we're going to randomly select a new note, play it, and add it to the played note array
+   //else give player error notice and replay sequence
+   //then listen for user input
+
+//strict mode
