81bfcc72b1f8f8a136eccddd24166e8b2f2359d0
Serafin <leaf466@gmail.com>
2017-01-03 16:00:56 -0800

    Add basic style layout

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 const express = require('express')
 const app = express()
 const pug = require('pug')
+const normalize = require('normalize')
 
-//app.set('view engine', 'pug')
-//app.set('/', __dirname + '/') //double check this
+app.set('view engine', 'pug')
+// app.set('/', __dirname + '/views') //double check this
+app.set('views', __dirname + 'views')
 
-//app.use('/', index)
+// app.use('/', index)
+app.use(express.static('public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello Tacos!')
+  // response.send('Hello Tacos!')
+  // response.render(index)
+  response.render(__dirname + '/views/index.pug')
 })
 
 app.listen(3000, function() {
