708fd6734a8bcb2a63f3dd28a8f0ccfceb3fa157
Serafin <leaf466@gmail.com>
2017-01-05 11:43:25 -0800

    before trial

--- public/controls.js
+++ public/controls.js
@@ -6,15 +6,13 @@ let gameRunning = true
 let currentPosition = 0
 
 const startGame = () => {
-  if(gameRunning) {
-    resetVars()
-  } else {
+  if(!gameRunning) {
     gameRunning = true
   }
-  // TODO: Make these run after player's turn and once before any turn
+  resetVars()
   simonAddNote()
   timeOutLoop(0, flashSounds, 1000)
-  playerTurn()
+  // TODO: Make these run after player's turn and once before any turn
 }
 
 const resetVars = () => {
@@ -37,14 +35,13 @@ function simonAddNote() {
   console.log(simonNotes)
 }
 
-const soundsToNotes = {
-  brown: document.getElementById("simonSound1"),
-  cyan: document.getElementById("simonSound2"),
-  indigo: document.getElementById("simonSound3"),
-  orange: document.getElementById("simonSound4")
-}
-
 function flashSounds(reps) {
+  const soundsToNotes = {
+    brown: document.getElementById("simonSound1"),
+    cyan: document.getElementById("simonSound2"),
+    indigo: document.getElementById("simonSound3"),
+    orange: document.getElementById("simonSound4")
+  }
   if(soundsToNotes[playerNotes[reps]]){
     soundsToNotes[playerNotes[reps]].play()
     $('#'+ [playerNotes[reps]]).effect('highlight', {}, 1000)
@@ -52,7 +49,7 @@ function flashSounds(reps) {
 }
 
 function timeOutLoop(reps, fn, delay) {
-  if(reps <= playerNotes.length) {
+  if(reps <= simonNotes.length) {
     // TODO: Delete const timerID
     const timerId = setTimeout(() => {
       fn(reps)
@@ -66,43 +63,53 @@ function playerTurn() {
     gameRunning = false
     resetVars()
     alert("You win! Press start to play again.")
-  } else if(playerNotes[currentPosition] !== simonNotes[currentPosition]) {
+    // Disable buttons
+  } else if(playerNotes[currentPosition] !== simonNotes[currentPosition]) { // Lose condition
     alert("You loose 😹, press start to try again!");
+    // Disable buttons
   } else {
-    let stillCorrect = true
-    playerNotes.forEach( (element, index) => {
-      if (element === simonNotes[index]) {
+    // Disable buttons
+    simonAddNote()
+    // Enable Error
 
-      } else {
-        stillCorrect = false
-      }
-    })
+    // let stillCorrect = true
+    // playerNotes.forEach( (element, index) => {
+    //   if (element === simonNotes[index]) {
+    //
+    //   } else {
+    //     stillCorrect = false
+    //   }
+    // })
   }
 }
 
 //disable player buttons
 $(document).ready(() => {
-  $('#orange').on('click', function() {
-    playerNotes.push("orange")
-    $('#simonSound4').get(0).play()
-    $('#orange').effect('highlight', {}, 250)
-  })
-  $('#cyan').on('click', function() {
-    playerNotes.push("cyan")
-    $('#simonSound2').get(0).play()
-    $('#cyan').effect('highlight', {}, 250)
-  })
-  $('#brown').on('click', function() {
-    playerNotes.push("brown")
-    $('#simonSound1').get(0).play()
-    $('#brown').effect('highlight', {}, 250)
-  })
-  $('#indigo').on('click', function() {
-    playerNotes.push("indigo")
-    $('#simonSound3').get(0).play()
-    $('#indigo').effect('highlight', {}, 250)
-  })
   $('#start').on('click', () => {
     startGame()
+    $('#orange').on('click', function() {
+      playerNotes.push("orange")
+      $('#simonSound4').get(0).play()
+      $('#orange').effect('highlight', {}, 250)
+      playerTurn()
+    })
+    $('#cyan').on('click', function() {
+      playerNotes.push("cyan")
+      $('#simonSound2').get(0).play()
+      $('#cyan').effect('highlight', {}, 250)
+      playerTurn()
+    })
+    $('#brown').on('click', function() {
+      playerNotes.push("brown")
+      $('#simonSound1').get(0).play()
+      $('#brown').effect('highlight', {}, 250)
+      playerTurn()
+    })
+    $('#indigo').on('click', function() {
+      playerNotes.push("indigo")
+      $('#simonSound3').get(0).play()
+      $('#indigo').effect('highlight', {}, 250)
+      playerTurn()
+    })
   })
 })
